3ccc90a623b0bdc88be5ac217f6d610fcf29447f
GoncaloCamaz <37366547+GoncaloCamaz@users.noreply.github.com>
2019-03-02 12:42:32 +0000

    Update InsertionDB.java
    Insert Experiment
    Insert Sample
    Insert Protocol

--- LightMonitor/app/src/main/java/com/example/lightmonitor/InsertionDB.java
+++ LightMonitor/app/src/main/java/com/example/lightmonitor/InsertionDB.java
@@ -3,6 +3,8 @@ package com.example.lightmonitor;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 public class InsertionDB
 {
@@ -46,6 +48,70 @@ public class InsertionDB
             Statement stmt = this.connection.establishConnection().createStatement();
             ResultSet res = stmt.executeQuery("INSERT INTO User (Name, Email, Password)\n" +
                     "VALUES (" + username + " ," +  email + " ," + password + ");");
+
+            if(res != null)
+            {
+                ret = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            return ret;
+        }
+    }
+
+    public boolean insertProtocol(String type, String text)
+    {
+        boolean ret = false;
+
+        try {
+            Statement stmt = this.connection.establishConnection().createStatement();
+            ResultSet res = stmt.executeQuery("INSERT INTO Protocol (Type, text)\n" +
+                    "VALUES (" + type + " ," +  text  + ");");
+
+            if(res != null)
+            {
+                ret = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            return ret;
+        }
+    }
+
+    public boolean insertSample(int experimentID, double latitude, double longitude, double luminusity, LocalDateTime date)
+    {
+        boolean ret = false;
+        Timestamp timestamp = Timestamp.valueOf(String.valueOf(date));
+
+        try {
+            Statement stmt = this.connection.establishConnection().createStatement();
+            ResultSet res = stmt.executeQuery("INSERT INTO Sample (Latitude, Longitude, Luminusity, TimeStamp, Experiment_ID)\n" +
+                    "VALUES (" + latitude + " ," +  longitude  + " ," + luminusity + " ," + timestamp + " ," + experimentID + ");");
+
+            if(res != null)
+            {
+                ret = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            return ret;
+        }
+    }
+
+    public boolean insertExperiment(String androidVersion, String brand, String model, int userId, int protocol)
+    {
+        boolean ret = false;
+
+        try {
+            Statement stmt = this.connection.establishConnection().createStatement();
+            ResultSet res = stmt.executeQuery("INSERT INTO Experiment (AndroidVersion, Brand, Model, User_ID, Protocol_ID)\n" +
+                    "VALUES (" + androidVersion + " ," +  brand  + " ," + model + " ," + userId + " ," + protocol + ");");
 
             if(res != null)
             {
